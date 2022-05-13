cd2bb83c25a5389887aeb97c79d1e08e71b5794c
Kingstalux <kingsleyngu25@gmail.com>
2022-05-13 02:55:38 +0100

    Add app componenet

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  get '*page', to: 'static#index', constraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
   root "static#index"
 end
