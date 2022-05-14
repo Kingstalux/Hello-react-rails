b7a97460af5649819ea6f59e205fe4b5f2161009
Kingstalux <kingsleyngu25@gmail.com>
2022-05-14 03:48:04 +0100

    Add greetings actions

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,16 +1,33 @@
 import React from "react"
-import PropTypes from "prop-types"
+import {connect} from "react-redux"
+import {createStructuredSelector} from "reselect"
+
+const GET_RANDOM_GREETINGS = "GET_RANDOM_GREETINGS"
+
+function getGreetings () {
+  console.log("getGreetings() Action!!")
+  return {
+    type: GET_RANDOM_GREETINGS
+  };
+};
+
 class Greeting extends React.Component {
   render () {
     return (
       <React.Fragment>
         Greeting: {this.props.greeting}
+
+        <button className="btn" onClick={() => this.props.getGreetings()}>Get Greetings</button>
       </React.Fragment>
     );
   }
 }
 
-Greeting.propTypes = {
-  greeting: PropTypes.string
-};
-export default Greeting
+const structuredSelector = createStructuredSelector({
+  greetings: state => state.greeting,
+})
+
+const mapDispatchToProps = {getGreetings};
+
+export default connect(structuredSelector, mapDispatchToProps)(Greeting);
+
